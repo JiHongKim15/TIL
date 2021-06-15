21ec647e21931e3f9566d0b838dab94a495424a2
JH <15computer@hanmail.net>
2021-06-15 22:13:56 +0900

    til

--- workspace/Study/src/BAEKJOON/두배열의합_2143.java
+++ workspace/Study/src/BAEKJOON/두배열의합_2143.java
@@ -1,43 +1,78 @@
 package BAEKJOON;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class ë‘ë°°ì—´ì˜í•©_2143 {
+public class µÎ¹è¿­ÀÇÇÕ_2143 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		int N = sc.nextInt();
 		int A[] = new int[N];
-		
-		for(int i=0; i<N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			A[i] = sc.nextInt();
 		}
-		
+
 		int M = sc.nextInt();
-		int B[] = new int[N];
-		
-		for(int i=0; i<M; i++) {
+		int B[] = new int[M];
+
+		for (int i = 0; i < M; i++) {
 			B[i] = sc.nextInt();
 		}
-		
+
 		int ans = 0;
+
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			arr.add(A[i]);
+			int sum = A[i];
+
+			for (int j = i + 1; j < N; j++) {
+				sum += A[j];
+				arr.add(sum);
+			}
+		}
+
+		ArrayList<Integer> brr = new ArrayList<>();
+
+		for (int i = 0; i < M; i++) {
+			brr.add(B[i]);
+			int sum = B[i];
+
+			for (int j = i + 1; j < M; j++) {
+				sum += B[j];
+				brr.add(sum);
+			}
+		}
+
+		Collections.sort(arr);
+		Collections.sort(brr);
+
+		int left = 0;
+		int right = brr.size() -1;
+
 		
-		for(int i=1; i< (1<<N); i++) {
-			for(int j=0; j<N; j++) {
-				if( (i & (1<<j)) != 0) {
-					System.out.print(A[j]);
-				}
+		// Áßº¹Ã³¸®
+		while (true) {
+			if (left == arr.size() || right < 0)
+				break;
+
+			int sum = arr.get(left) + brr.get(right);
+			if (sum == T) {
+				ans++;
+				if(left == arr.size()-1) right--;
+				else left++;
 			}
-			System.out.println();
+			else if (sum < T)
+				left++;
+			else
+				right--;
 		}
-		System.out.println("--");
-		for(int i=1; i< (1<<M); i++) {
-			for(int j=0; j< M; j++) {
-				if( (i& (1<<j)) != 0) {
-					System.out.print(B[j]);
-				}
-			}
-			System.out.println();
-		}
+		
+		System.out.println(ans);
+
 	}
 }
--- workspace/Study/src/BAEKJOON/소수의연속합_1644.java
+++ workspace/Study/src/BAEKJOON/소수의연속합_1644.java
@@ -1,57 +1,50 @@
 package BAEKJOON;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class 소수의연속합_1644 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		
-		
-		HashSet<Integer> hs = new HashSet<>();
-		for (int i = 2; i <= N; i++) {
-			hs.add(i);
+
+		boolean[] check = new boolean[N + 1];
+
+		for (int i = 2; i * i <= N; i++) {
+			for (int j = i * i; j <= N; j += i) {
+				check[j] = true;
+			}
 		}
-		
-		for(int i=2; i*i<=N; i++) {
-            for(int j=i*i; j<=N; j+=i) {
-            	hs.remove(j);
-            }
-        }
-		
-		
+
 		int result = 0;
-		
-		
-		int[] arr = new int[hs.size()];
-		
-		int j = 0;
-		for(Integer i : hs) {
-			arr[j++] = i;
+
+		ArrayList<Integer> ll = new ArrayList<>();
+
+		for (int i = 2; i <= N; i++) {
+			if (!check[i]) {
+				ll.add(i);
+			}
+
 		}
-		
-		Arrays.sort(arr);
+
 		int sum = 0;
 		int left = 0;
-		int right= 0;
-		
-		while(right >= left && right <arr.length && left<arr.length) {
-			sum = sum + arr[right++];
-			if(sum >N) {
-				while(sum > N){
-					sum -= arr[left];
-					left++;
-				}
-			}
-			if(sum ==N) {
+		int right = 0;
+
+		while (true) {
+
+			if (sum >= N) {
+				sum -= ll.get(left++);
+			} else if (right == ll.size())
+				break;
+			else
+				sum += ll.get(right++);
+			if (sum == N) {
 				result++;
 			}
 		}
-		
+
 		System.out.println(result);
-		
-		
+
 	}
 }
